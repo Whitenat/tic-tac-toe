11bd4151ad461e950401ed461d9de50377a41cfe
Natashia White <whitenat@oregonstate.edu>
2020-06-09 09:40:55 -0700

    make sure board has locations attribute

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,28 @@
 require_relative '../lib/board'
 
 describe 'A Tic Tac Toe board' do
+	let(:full_board) {
+    Board.new({
+      top: {left: :fake, middle: :fake, right: :fake},
+      middle: {left: :fake, middle: :fake, right: :fake},
+      bottom: {left: :fake, middle: :fake, right: :fake}
+    })
+  }
+
+  let(:midgame_board) {
+    Board.new({
+      top: {left: :o, middle: nil, right: nil},
+      middle: {left: nil, middle: :x, right: nil},
+      bottom: {left: nil, middle: nil, right: nil}
+    })
+  }
 
   it 'exists' do
     Board.new
+  end
+
+  it 'has locations' do
+  	expect(Board.new).to respond_to(:locations)
   end
 
 end
